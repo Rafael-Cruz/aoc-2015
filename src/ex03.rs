c720a883ff9b93b8ef36843db3662fb8aa30c4e1
Rafael Cruz <rafael.rblc@gmail.com>
2025-04-10 22:26:04 -0300

    add exercise 03 part 2 answer

--- src/ex03.rs
+++ src/ex03.rs
@@ -11,12 +11,13 @@ pub fn run() {
 
     trim_newline(&mut contents);
 
-    let result = calc_delivered_houses(&contents);
+    let santa_result = get_santa_houses_qty(&contents);
+    let robosanta_result = get_robosanta_houses_qty(&contents);
 
-    println!("Result: {}", result);
+    println!("Santa: {}; Robo-Santa: {}", santa_result, robosanta_result);
 }
 
-fn calc_delivered_houses(input: &str) -> usize {
+fn get_santa_houses_qty(input: &str) -> usize {
     let mut visited_houses: Vec<Point> = vec![Point(0, 0)];
     let mut last_pos: usize = 0;
 
@@ -53,6 +54,47 @@ fn calc_delivered_houses(input: &str) -> usize {
     return result;
 }
 
+fn get_robosanta_houses_qty(input: &str) -> usize {
+    let mut visited_houses: Vec<Point> = vec![Point(0, 0)];
+    let mut odd_last_pos: usize = 0;
+    let mut even_last_pos: usize = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        let is_even = i % 2 == 0;
+        let last_pos: &mut usize = if is_even { &mut even_last_pos } else { &mut odd_last_pos };
+
+        let new_point = match c {
+            '>' => Point(visited_houses[*last_pos].0, visited_houses[*last_pos].1 + 1),
+            '<' => Point(visited_houses[*last_pos].0, visited_houses[*last_pos].1 - 1),
+            '^' => Point(visited_houses[*last_pos].0 + 1, visited_houses[*last_pos].1),
+            'v' => Point(visited_houses[*last_pos].0 - 1, visited_houses[*last_pos].1),
+            _ => {
+                println!("caractere {} n reconhecido no input", c);
+                panic!("caractere n reconhecido")
+            }
+        };
+
+        let mut duplicate = false;
+
+        for (j, point) in visited_houses.iter().enumerate() {
+            if new_point == *point {
+                duplicate = true;
+                *last_pos = j;
+                break;
+            }
+        }
+
+        if !duplicate {
+            visited_houses.push(new_point);
+            *last_pos = visited_houses.len() - 1;
+        }
+    }
+
+    let result = visited_houses.len();
+
+    return result;
+}
+
 fn trim_newline(s: &mut String) {
     if s.ends_with('\n') {
         s.pop();
@@ -66,9 +108,16 @@ fn trim_newline(s: &mut String) {
 mod test {
     use super::*;
     #[test]
-    fn test_calc_delivered_houses() {
-        assert_eq!(calc_delivered_houses(">"), 2);
-        assert_eq!(calc_delivered_houses("^>v<"), 4);
-        assert_eq!(calc_delivered_houses("^v^v^v^v^v"), 2);
+    fn test_santa_delivery() {
+        assert_eq!(get_santa_houses_qty(">"), 2);
+        assert_eq!(get_santa_houses_qty("^>v<"), 4);
+        assert_eq!(get_santa_houses_qty("^v^v^v^v^v"), 2);
+    }
+
+    #[test]
+    fn test_robosanta_delivery() {
+        assert_eq!(get_robosanta_houses_qty("^v"), 3);
+        assert_eq!(get_robosanta_houses_qty("^>v<"), 3);
+        assert_eq!(get_robosanta_houses_qty("^v^v^v^v^v"), 11);
     }
 }
