2c049fd42ae2c874d873b636778764983e5ef71d
Rafael Cruz <rafael.rblc@gmail.com>
2025-04-11 23:47:12 -0300

    exercise 04 (unoptimized)

--- src/ex04.rs
+++ src/ex04.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 use md5;
 
 const N_LIMIT: u64 = 1844674407370955200;
@@ -7,15 +5,51 @@ const KEY_PREFIX: &str = "iwrupvqb";
 
 pub fn run() {
     let mut n: u64 = 1;
-    // loop {
-        // if n >= N_LIMIT {
-        //     println!("limite atingido");
-        //     break;
-        // }
+    loop {
+        if n >= N_LIMIT {
+            println!("limite atingido");
+            break;
+        }
 
-        let mut key: String = String::from(KEY_PREFIX);
-        key.push_str(n.to_string().as_str());
+        let hash = generate_hash(n);
+        
+        if verify_hash(&hash) {
+            println!("n = {}; hash: {}", n, hash);
+            break;
+        }
+        
+        n = n + 1;
+    }
+}
 
-        let hash = md5::compute(key);
-    // }
+fn generate_hash(n: u64) -> String {
+    let mut key: String = String::from(KEY_PREFIX);
+    key.push_str(n.to_string().as_str());
+
+    let hash = md5::compute(key);
+    let number: u128 = u128::from_be_bytes(*hash);
+    let formatted_number = format!("{:x}", number);
+
+    return formatted_number
+}
+
+fn verify_hash(hash: &str) -> bool {
+    &hash[..5] == "00000"
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn test_generates_hash() {
+        assert_eq!(generate_hash(1), "6a76d07a5b54f2061b3bb4afb7433949");
+        assert_eq!(generate_hash(2), "6cff6c82636bb0caead7381126553037");
+    }
+
+    #[test]
+    fn test_verify_hash() {
+        assert_eq!(verify_hash("6a76d07a5b54f2061b3bb4afb7433949"), false);
+        assert_eq!(verify_hash("0000087a5b54f2061b3bb4afb7433949"), true);
+        assert_eq!(verify_hash("0000787a5b54f2061b3bb4afb7433949"), false);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
+
 mod ex03;
 mod ex04;
 
