e4d5914eaf1722f7d39fb9a9efd3308b42a99d3c
Rafael Cruz <rafael.rblc@gmail.com>
2025-04-13 19:02:36 -0300

    Exercise 04: Little optimization

--- src/ex04.rs
+++ src/ex04.rs
@@ -14,27 +14,26 @@ pub fn run() {
         let hash = generate_hash(n);
         
         if verify_hash(&hash) {
-            println!("n = {}; hash: {}", n, hash);
+            println!("n = {}; hash: {:?}", n, hash);
             break;
+        }
+
+        if n % 10 == 0 {
+            println!("{}", n);
         }
         
         n = n + 1;
     }
 }
 
-fn generate_hash(n: u64) -> String {
-    let mut key: String = String::from(KEY_PREFIX);
-    key.push_str(n.to_string().as_str());
-
-    let hash = md5::compute(key);
-    let number: u128 = u128::from_be_bytes(*hash);
-    let formatted_number = format!("{:x}", number);
-
-    return formatted_number
+fn generate_hash(n: u64) -> md5::Digest {
+    md5::compute([KEY_PREFIX, n.to_string().as_str()].concat())
 }
 
-fn verify_hash(hash: &str) -> bool {
-    &hash[..5] == "00000"
+fn verify_hash(hash: &md5::Digest) -> bool {
+    hash[0] == 0 &&
+    hash[1] == 0 &&
+    hash[2] < 16
 }
 
 #[cfg(test)]
@@ -42,14 +41,14 @@ mod tests{
     use super::*;
     #[test]
     fn test_generates_hash() {
-        assert_eq!(generate_hash(1), "6a76d07a5b54f2061b3bb4afb7433949");
-        assert_eq!(generate_hash(2), "6cff6c82636bb0caead7381126553037");
+        assert_eq!(generate_hash(1), md5::Digest(0x6a76d07a5b54f2061b3bb4afb7433949_u128.to_be_bytes()) );
+        assert_eq!(generate_hash(2), md5::Digest(0x6cff6c82636bb0caead7381126553037_u128.to_be_bytes()) );
     }
 
     #[test]
     fn test_verify_hash() {
-        assert_eq!(verify_hash("6a76d07a5b54f2061b3bb4afb7433949"), false);
-        assert_eq!(verify_hash("0000087a5b54f2061b3bb4afb7433949"), true);
-        assert_eq!(verify_hash("0000787a5b54f2061b3bb4afb7433949"), false);
+        assert_eq!(verify_hash(&md5::Digest(0x6a76d07a5b54f2061b3bb4afb7433949_u128.to_be_bytes())), false);
+        assert_eq!(verify_hash(&md5::Digest(0x0000087a5b54f2061b3bb4afb7433949_u128.to_be_bytes())), true);
+        assert_eq!(verify_hash(&md5::Digest(0x0000787a5b54f2061b3bb4afb7433949_u128.to_be_bytes())), false);
     }
 }
